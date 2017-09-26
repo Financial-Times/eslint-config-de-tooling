492d6ec96d4087f911f51dec45dc80c8c6d38135
Charlie Briggs <charlie.briggs@bbc.co.uk>
2017-09-26 12:27:52 +0100

    Modify rules from airbnb base
    This will not be compatible with prettier, prettier to be extracted in future

--- index.js
+++ index.js
@@ -6,5 +6,26 @@ module.exports = {
     ],
     "plugins": [
         "prettier"
-    ]
+    ],
+    "ecmaFeatures": {
+        "impliedStrict": false
+    },
+    "rules": {
+        'arrow-body-style': 0,
+        'arrow-parens': [2, "always"],
+        'func-names': 0,
+        'import/no-extraneous-dependencies': [2, {'devDependencies': ['test/**/*.js', '**/*.test.js', '**/*.spec.js', 'gulpfile.js', 'gulp-tasks/**/*.js']}],
+        'import/no-named-as-default': 0, // doesn't work with decorated defaults https://github.com/benmosher/eslint-plugin-import/issues/544
+        'import/prefer-default-export': 0,
+        'indent': [2, 4],
+        'max-len': 1,
+        'new-cap':  [2, {capIsNewExceptions: ['Router']}],
+        'no-param-reassign': 0,
+        'no-underscore-dangle': [2, {allowAfterThis: true}],
+        'object-curly-spacing': [2, 'never'],
+        'padded-blocks': 0,
+        'prefer-arrow-callback': [2, {allowNamedFunctions: true}],
+        'quote-props': [2, 'as-needed', {numbers: true}],
+        'space-before-function-paren': [2, 'never']
+    }
 }
