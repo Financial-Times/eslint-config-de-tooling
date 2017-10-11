9b63a009436efdb344b87ff5418aa2b591a3b770
Charlie Briggs <charlie.briggs@ft.com>
2017-10-11 12:48:14 +0100

    Loosen import/no-extraneous-dependencies
    This now treats public + webpack/rollup/jest/gulp config as dev dependencies
    
    Also add aws-sdk as a core module as it's bundled on lambda

--- index.js
+++ index.js
@@ -11,12 +11,34 @@ module.exports = {
             experimentalObjectRestSpread: false,
         },
     },
+    'settings': {
+        'import/core-modules': [
+            'aws-sdk',
+        ],
+    },
     'rules': {
         'arrow-body-style': 'off',
         'func-names': 'off',
         'import/no-extraneous-dependencies': ['error', {
             'devDependencies': [
-                'test/**/*.js', '**/*.test.js', '**/*.spec.js', 'gulpfile.js', 'gulp-tasks/**/*.js'
+                'public', // public static asset paths
+                'test/**', // tape, common npm pattern
+                'tests/**', // also common npm pattern
+                'spec/**', // mocha, rspec-like pattern
+                '**/__tests__/**', // jest pattern
+                'test.{js,jsx}', // repos with a single test file
+                'test-*.{js,jsx}', // repos with multiple top-level test files
+                '**/*.{test,spec}.{js,jsx}', // tests where the extension denotes that it is a test
+                '**/jest.config.js', // jest config
+                '**/webpack.config.js', // webpack config
+                '**/webpack.config.*.js', // webpack config
+                '**/rollup.config.js', // rollup config
+                '**/rollup.config.*.js', // rollup config
+                '**/gulpfile.js', // gulp config
+                '**/gulpfile.*.js', // gulp config
+                '**/Gruntfile{,.js}', // grunt config
+                '**/protractor.conf.js', // protractor config
+                '**/protractor.conf.*.js', // protractor config
             ]
         }],
         'import/no-named-as-default': 'off', // doesn't work with decorated defaults https://github.com/benmosher/eslint-plugin-import/issues/544
