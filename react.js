9d8b507dca47679347e6c4201d9bfd1bd2599d46
Charlie Briggs <charlie.briggs@ft.com>
2018-12-06 16:29:10 +0000

    Override a couple of undesirable react rules, non-breaking as only loosening

--- react.js
+++ react.js
@@ -1,3 +1,20 @@
 module.exports = {
     extends: ['airbnb', './common.js'],
+    rules: {
+        // this can be extremely annoying when only using one prop, and is stylistic
+        // https://github.com/yannickcr/eslint-plugin-react/blob/843d71a432baf0f01f598d7cf1eea75ad6896e4b/docs/rules/destructuring-assignment.md
+        'react/destructuring-assignment': 'off',
+        // override to allow origami style controls, which do not nest controls in labels
+        // https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/b800f40a2a69ad48015ae9226fbe879f946757ed/docs/rules/label-has-associated-control.md
+        'jsx-a11y/label-has-associated-control': [
+            'error',
+            {
+                labelComponents: [],
+                labelAttributes: [],
+                controlComponents: [],
+                assert: 'either',
+                depth: 25,
+            },
+        ],
+    },
 };
