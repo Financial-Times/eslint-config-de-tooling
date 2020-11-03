a5fbc6697e91bfac4fb4f6f831c9cc2f80884652
Rhys Evans <rhys.evans@ft.com>
2020-11-03 09:25:34 +0000

    allow variables in test descriptions

--- rules/jest.js
+++ rules/jest.js
@@ -5,11 +5,16 @@ const { usingJsx } = require('../lib/feature-detect');
 const config = {
 	extends: ['plugin:jest/recommended'],
 	rules: {
-		// This disallows .skip, which is auseful tool for writing skelton tests
+		// This disallows .skip, which is a useful tool for writing skelton tests
 		// prior to full implementation. no-focused-tests is still enabled, which
 		// catches .only - that's the killer
 		'jest/no-disabled-tests': [0, { extensions: ['.js', '.jsx'] }],
-
+		
+		// This rule disallows using template strings with variables as description 
+		// names, which is useful when e.g. running the same set of tests with multiple
+		// configurations
+		'jest/valid-describe': 'off',
+		
 		// Disable as it's frequently necessary to do inline requires
 		// when working with jest mocks
 		'global-require': 'off',
